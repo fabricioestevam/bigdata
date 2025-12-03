064d4d3085f46cc8271d536e7e8e2d044e38c81a
fabricioestevam <fabricio.junior@edu.pe.senac.br>
2025-12-03 04:13:07 +0000

    chore: remove DHT22 sensor files

---
+++ cliente_webcam.py
@@ -0,0 +1,8 @@
+# Se servidor estiver no Codespace:
+SERVIDOR_URL = "https://effective-halibut-vpvqv7w59rhxx65-5000.app.github.dev"
+
+# Se servidor estiver local:
+SERVIDOR_URL = "http://localhost:5000"
+
+# Timing
+INTERVALO_CAPTURA = 5  # Ajustar conforme necessário
---
+++ server_yolo.py
@@ -0,0 +1,2 @@
+# Linhas válidas (suas linhas reais!)
+LINHAS_VALIDAS = ["437", "2441", "2450", "2444"]
